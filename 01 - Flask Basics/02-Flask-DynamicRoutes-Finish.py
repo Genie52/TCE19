f9163c9fa53284b6ae0a90adf290f82785ecec5e
Genie52 <vnadinic@yahoo.com>
2019-06-10 18:23:12 +0100

    Add files via upload

--- 01 - Flask Basics/02-Flask-DynamicRoutes-Finish.py
+++ 01 - Flask Basics/02-Flask-DynamicRoutes-Finish.py
@@ -1,19 +1,29 @@
+# import Flask library (class) that has needed functionality to build Web Server
 from flask import Flask
+
+# next we create an instance of this (Flask) class
 app = Flask(__name__)
 
+# we then use the route() decorator to tell Flask what URL should trigger our function
+# this is out root page!
 @app.route('/')
 def index():
     return '<h1>Hello Tabloids from TCE 19 Berlin!</h1>' #this is actual HTML code!
 
-@app.route('/tableauinfo') #this is the URL path we gonna use to access this page!
+# this is the URL path we gonna use to access this page! eg. http://localhost:5000/tableauinfo
+@app.route('/tableauinfo')
 def tableauinfo():
     return '<h1>Tableau is the Best!</h1>'
 
+# You can add variable sections to a URL by marking sections with <variable_name>.
+# Your function then receives the <variable_name> as a keyword argument.
 #------- PASTE CODE HERE!! <<< START >>> ------------------
-@app.route('/participant/<name>') #this is a dynamic URL that expects a parameter
+# this is a dynamic URL that expects a parameter - eg. http://localhost:5000/participant/Sophie
+@app.route('/participant/<name>')
 def participant(name):
     return '<h1>Hello {}, welcome to TCE 19!<h1>'.format(name)
 #------- PASTE CODE HERE!! <<< END >>> ------------------
 
+# this just means RUN THE APP!! (our web server)
 if __name__ == '__main__':
     app.run()
