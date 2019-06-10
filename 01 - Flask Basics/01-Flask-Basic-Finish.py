d28f7136f3ebc656da369b52608278402be92dd4
Genie52 <vnadinic@yahoo.com>
2019-06-10 15:59:10 +0100

    Add files via upload

--- 01 - Flask Basics/01-Flask-Basic-Finish.py
+++ 01 - Flask Basics/01-Flask-Basic-Finish.py
@@ -1,10 +1,16 @@
+# import Flask library (class) that has needed functionality to build Web Server
 from flask import Flask
 
+# next we create an instance of this (Flask) class
 app = Flask(__name__)
 
-#this is our start page!
+# we then use the route() decorator to tell Flask what URL should trigger our function
+# this is out root page!
 @app.route('/')
 
+# The function is given a name (it can be any function name! - in this case we named it 'index')
+# which is also used to generate URLs for that particular function,
+# and returns the message we want to display in the user’s browser.
 #------- PASTE CODE HERE!! <<< START >>> ------------------
 def index():
     return '<h1>Hello Tabloids from TCE 19 Berlin!</h1>' #this is actual HTML code!
